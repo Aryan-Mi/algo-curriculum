4a9c4bd8b030e3bc2a714f1f4fa138e25af7894f
Aryan Mirzazadeh <54137005+Aryan-Mi@users.noreply.github.com>
2023-11-26 19:51:32 +0100

    created a content directory for contentlayer * also added some example mdx files

--- src/app/docs/[lesson]/page.tsx
+++ src/app/docs/[lesson]/page.tsx
@@ -1,11 +1,44 @@
 import LessonTitle from './lesson-title'
+import { allLessons, Lesson } from 'contentlayer/generated'
+import { compareDesc, compareAsc, format, parseISO } from 'date-fns'
+import Link from 'next/link'
 
 type Props = {}
 
 export default function Lesson({}: Props) {
+  const lessons = allLessons.sort((a, b) =>
+    compareAsc(new Date(a.date), new Date(b.date)),
+  )
   return (
     <>
-      <LessonTitle />
+      {/* <LessonTitle /> */}
+      <div>
+        {lessons.map((lesson, idx) => (
+          <LessonCard key={idx} {...lesson} />
+        ))}
+      </div>
     </>
+  )
+}
+
+function LessonCard(lesson: Lesson) {
+  return (
+    <div className="mb-8">
+      <h2 className="mb-1 text-xl">
+        <Link
+          href={lesson.url}
+          className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
+        >
+          {lesson.title}
+        </Link>
+      </h2>
+      <time dateTime={lesson.date} className="mb-2 block text-xs text-gray-600">
+        {format(parseISO(lesson.date), 'LLLL d, yyyy')}
+      </time>
+      <div
+        className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
+        dangerouslySetInnerHTML={{ __html: lesson.body.html }}
+      />
+    </div>
   )
 }
