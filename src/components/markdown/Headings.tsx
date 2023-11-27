909922e91801952903e3576182e88388ee04f18a
Aryan Mirzazadeh <54137005+Aryan-Mi@users.noreply.github.com>
2023-11-27 23:20:16 +0100

    added some basic styling to different heading levels * installed tw-merge package for resolving tailwing clashes * NOTE: added a temporary "override" in package.json to stop contentlayer from throwing an error when installing new packages. (Until it is compatible with next ^14)

--- src/components/markdown/Headings.tsx
+++ src/components/markdown/Headings.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 const headingLevels = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as const
+import { twMerge } from 'tailwind-merge'
 
 type DefaultHeading = React.HTMLAttributes<HTMLHeadingElement>
 type HeadingProp = React.HTMLAttributes<HTMLHeadingElement> & {
@@ -13,8 +14,21 @@ type MDXComponent = {
 }
 
 function Heading({ level, children, ...rest }: HeadingProp) {
-  const styling = { className: 'font-bold text-xl text-blue-700' }
-  return React.createElement(level, { ...styling, ...rest }, children)
+  const baseStyle = 'font-normal text-base text-blue-500'
+  const headingStyling = {
+    h1: 'font-bold text-2xl text-blue-500',
+    h2: 'font-bold text-xl text-blue-600',
+    h3: 'text-lg font-medium text-blue-500',
+    h4: 'text-blue-500',
+    h5: '',
+    h6: '',
+  }
+
+  return React.createElement(
+    level,
+    { className: twMerge(baseStyle, headingStyling[level]), ...rest },
+    children,
+  )
 }
 
 function createMDXHeadings() {
@@ -26,7 +40,6 @@ function createMDXHeadings() {
     )
     return acc
   }, {} as MDXComponent)
-  console.log('MDX Components', mdxComponents.h1.toString())
   return mdxComponents
 }
 
